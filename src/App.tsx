677e98ba176b4bd272b2238db0ad8b4cacfc04a1
Tae.Garam <drasla@naver.com>
2022-12-24 01:59:57 +0900

    #5.10 React Query part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -77,6 +78,7 @@ export const App = () => {
         <>
             <GlobalStyle />
             <Router />
+            <ReactQueryDevtools initialIsOpen={true} />
         </>
     );
 };
